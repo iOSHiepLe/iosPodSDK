1a051aceb801209e22c920bcce1ebee7e514619b
HiepLe <hieplemobile@gmail.com>
2022-08-26 08:18:40 +0700

    Initial Commit

---
+++ iosPodSDK/iosPodSDK.h
@@ -0,0 +1,18 @@
+//
+//  iosPodSDK.h
+//  iosPodSDK
+//
+//  Created by Le Minh Hiep on 26/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iosPodSDK.
+FOUNDATION_EXPORT double iosPodSDKVersionNumber;
+
+//! Project version string for iosPodSDK.
+FOUNDATION_EXPORT const unsigned char iosPodSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosPodSDK/PublicHeader.h>
+
+
